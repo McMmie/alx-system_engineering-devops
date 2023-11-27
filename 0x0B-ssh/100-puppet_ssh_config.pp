889a9db3829ad6da41788c4b64174b7a7c9a6c13
McMmie <tharausamuel01@gmail.com>
2023-11-27 01:51:41 +0000

    update

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,11 +3,11 @@ file_line { 'identity file':
   ensure => present,
   path   => '/etc/ssh/sshd_config',
   line   => 'IdentityFile ~/.ssh/school',
-  match  => '^IdentityFile',
+  replace=> True,
 }
 file_line { 'Turn off passwd auth':
   ensure => present,
   path   => '/etc/ssh/sshd_config',
   line   => 'PasswordAuthentication no',
-  match  => '^#?PasswordAuthentication',
+  replace=> True,
 }
