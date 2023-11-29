5d646b9b173582a64e226f4e8388949cc5d5d6c1
McMmie <tharausamuel01@gmail.com>
2023-11-29 00:32:21 +0000

    configured nginx using puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -4,19 +4,26 @@ include nginx
 
 package { 'nginx':
   ensure  => 'present',
-  require => 'apt'
+  require => Apt['nginx'],
 }
+nginx::resource::server { '54.237.38.25':
+  listen port => 80,
 file { '/usr/share/nginx/html/index.html':
   ensure  => present,
   content => 'Hello World',
 }
 
 nginx::resource::location { '54.237.38.25/redirect_me/':
-  ensure   => present,
-  location => '/path/',
-  server   => '54.237.38.25',
+  ensure          => present,
+  location        => '/redirect_me/',
+  server          => '54.237.38.25',
   location_config => {
     'return => 301 https://open.spotify.com/track/6PwjKdyaavh0KaOsU6KKYJ?si=395f4c65e10b4772;',
   },
+}
+
+service { 'nginx':
+  ensure => 'running',
+  enable => true,
 }
 
