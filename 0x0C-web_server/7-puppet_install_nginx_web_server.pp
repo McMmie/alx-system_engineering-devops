a6c764ad432772932d07afe17d7a4c4aa1468bd2
McMmie <tharausamuel01@gmail.com>
2023-11-29 01:17:16 +0000

    refactored and debugged

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,10 +1,17 @@
 #Installing packages using puppet
 
 include nginx
+class nginx_installer {
+  package { 'nginx':
+    ensure  => installed,
+  }
+}
 
-package { 'nginx':
-  ensure  => installed,
-  require => 'apt',
+node 'stapp01.stratos.xfusioncorp.com', 'stapp02.stratos.xfusioncorp.com',
+'stapp03.stratos.xfusioncorp.com' {
+
+  include nginx_installer
+
 }
 
 service { 'nginx':
