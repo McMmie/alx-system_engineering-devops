47227f1fe89762444a4ac84e7134e89600f4985c
McMmie <tharausamuel01@gmail.com>
2024-01-02 21:25:37 +0000

    created a dictionary of list of dictionaries

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -10,11 +10,13 @@ import requests
 def all_to_json():
     """return API data"""
     USERS = []
-    userss = requests.get("http://jsonplaceholder.typicode.com/users")
-    for u in userss.json():
+    users = requests.get("http://jsonplaceholder.typicode.com/users")
+
+    for u in users.json():
         USERS.append((u.get('id'), u.get('username')))
     TASK_STATUS_TITLE = []
     todos = requests.get("http://jsonplaceholder.typicode.com/todos")
+
     for t in todos.json():
         TASK_STATUS_TITLE.append((t.get('userId'),
                                   t.get('completed'),
@@ -22,13 +24,16 @@ def all_to_json():
 
     """export to json"""
     data = dict()
-    for u in USERS:
-        t = []
+
+    for usr in USERS:
+        lst = []
         for task in TASK_STATUS_TITLE:
-            if task[0] == u[0]:
-                t.append({"task": task[2], "completed": task[1],
-                          "username": u[1]})
-        data[str(u[0])] = t
+
+            if task[0] == usr[0]:
+                lst.append({"task": task[2], "completed": task[1],
+                           "username": usr[1]})
+
+        data[str(usr[0])] = lst
     filename = "todo_all_employees.json"
     with open(filename, "w") as f:
         json.dump(data, f, sort_keys=True)
